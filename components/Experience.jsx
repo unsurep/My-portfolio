94a07ead9e0ad804492074487248b3c92c5adca7
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-01-27 06:15:57 +0100

    experience card section

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,12 +1,26 @@
+'use client'
 import React from 'react'
+import {motion} from 'motion/react'
+import ExperienceCard from './ExperienceCard.'
 
 const WorkExperience = () => {
   return (
-    <div id='experience' className='snap-center h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+    <motion.div 
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:1.5}}
+      id='experience' className='snap-center h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
       <h3  className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
 
+      {/* ExperienceCard section */}
+      <div>
+        <ExperienceCard/>
+        <ExperienceCard/>
+        <ExperienceCard/>
+      </div>
+
       
-    </div>
+    </motion.div>
   )
 }
 
---
+++ components/ExperienceCard..jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const ExperienceCard = () => {
+  return (
+    <div>
+      Exprncecard
+    </div>
+  )
+}
+
+export default ExperienceCard
